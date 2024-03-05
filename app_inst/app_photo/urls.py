1b6011c85dd1c3c51f81288179d701d664ca6ffd
VladSkopenko <skopirka2k17@gmail.com>
2024-03-05 22:35:06 +0300

    Added style

--- app_inst/app_photo/urls.py
+++ app_inst/app_photo/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
-app_name = "app_photo" # название переменной обяхательно должно быть app_name
+app_name = "app_photo"  # название переменной обяхательно должно быть app_name
 urlpatterns = [
-    path("", views.index, name="home"),   #app_photo:home
+    path("", views.index, name="home"),  # app_photo:home
 ]
