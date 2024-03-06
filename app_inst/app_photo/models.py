5227802262d3d88c849ceed859e0f01b5869b29f
VladSkopenko <skopirka2k17@gmail.com>
2024-03-06 14:18:07 +0300

    added model

--- app_inst/app_photo/models.py
+++ app_inst/app_photo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Picture(models.Model):
+    description = models.CharField(max_length=300)
+    path = models.ImageField(upload_to="images")
+
