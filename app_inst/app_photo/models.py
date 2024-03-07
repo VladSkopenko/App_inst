38b7e887cd89bf1c96565a2b2ab915d79496f519
VladSkopenko <skopirka2k17@gmail.com>
2024-03-07 19:10:38 +0300

    added validate by size

--- app_inst/app_photo/models.py
+++ app_inst/app_photo/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_file_size(value):
+    filesize = value.size
+    if filesize > 1_000_000:
+        raise ValidationError("Max size 1mb")
+    else:
+        return value
 
 
 class Picture(models.Model):
     description = models.CharField(max_length=300)
-    path = models.ImageField(upload_to="images")
+    path = models.ImageField(upload_to="images", validators=[validate_file_size])
 
